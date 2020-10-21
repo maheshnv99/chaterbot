8a994183f4c8a6a0b4f51c5c829641396a86a87e
maheshnv99 <mahesh.nandini66@gmail.com>
2020-10-21 21:25:58 +0530

    Updates

---
+++ precache-manifest.d1e63954d7d0e905c15e250f521c5d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60af23d9496edac74d693f4ce0f8aa48",
+    "url": "/chaterbot/index.html"
+  },
+  {
+    "revision": "5ee34c2b5f173af949a7",
+    "url": "/chaterbot/static/css/main.c84e8df1.chunk.css"
+  },
+  {
+    "revision": "799811a889c470c17404",
+    "url": "/chaterbot/static/js/2.82d1039f.chunk.js"
+  },
+  {
+    "revision": "aa29a360d3cedd1560747d2451d7ec0e",
+    "url": "/chaterbot/static/js/2.82d1039f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee34c2b5f173af949a7",
+    "url": "/chaterbot/static/js/main.02870479.chunk.js"
+  },
+  {
+    "revision": "0dae816277a0639b668e",
+    "url": "/chaterbot/static/js/runtime-main.9b575f99.js"
+  }
+]);
